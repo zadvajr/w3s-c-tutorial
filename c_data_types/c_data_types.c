e3b3603f5b0b2e118045e6ab0022057f2567e08a
zadvajr <danielzadva@gmail.com>
2023-06-01 21:37:54 +0100

    Added c_data_types.c and its executables

---
+++ c_data_types/c_data_types.c
@@ -0,0 +1,19 @@
+//This example shows c data types
+
+#include <stdio.h>
+
+int main() {
+    //variable declarations and initializations
+    int myInteger = 34;
+    float myFloat = 22.0;
+    char myChar = 'Z';
+    double myDouble = 22.34000;
+
+    //prints variable
+    printf("My Integer is: %d\n", myInteger);
+    printf("My Float is: %f\n", myFloat);
+    printf("My Character is: %c\n", myChar);
+    printf("My Double is: %lf\n", myDouble);
+
+    return 0;
+}
