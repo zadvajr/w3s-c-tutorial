38924f8771f711124d86ea54eeabb07a59a955da
zadvajr <danielzadva@gmail.com>
2023-06-24 10:40:04 +0100

    c variables

---
+++ c-variables/variables.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * c-variables - 
+ */
+int main(void)
+{
+	int age; /** variable age is declared */
+
+	age = 34; /* age is initialized */
+	
+	printf("I am %d years old.", age); /** combine text and value of age in single
+					    * printf function using a format specifier
+					    */
+	return (0);
+}
