4b5a9a09a5b5a0b868428e8877b046594dd7ce79
zadvajr <danielzadva@gmail.com>
2023-06-24 10:11:55 +0100

    c output

---
+++ c-output/printf_function.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *
+ * printf - printf functons prints output to standard output which is the screen
+ *
+ * printf - does not add newline to the output
+ *
+ */
+int main(void)
+{
+	printf("Hello World");
+	printf("I am learning C Programming Language");
+
+	return (0);
+}
